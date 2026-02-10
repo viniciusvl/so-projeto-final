50e6c38f0bb6aa536140e4927666030fac618e71
Vinicius Victor <viniciusvictor443@gmail.com>
2026-02-10 11:44:49 -0300

    finish: loader.s

--- loader.s
+++ loader.s
@@ -1,6 +1,20 @@
-global loader                   
+global loader                   ; Aqui informa onde o arquivo começa
+                                ; armazena um endereço de memória que aponta para as instruções          
 
 MAGIC_NUMBER equ 0x1BADB002     ; Define o número mágico lido pelo GRUB
 FLAGS        equ 0x0            ; Configurações do GRUB
 CHECKSUM     equ -MAGIC_NUMBER  ; Interamente, o GRUB verifica o número por MAGIC_NUMBER + CHECKSUM + FLAGS = 0
+
+section .text:                  ; Indica para o Assembler onde o código começa
+align 4                         ; Avisa para o Assembler começar em endereços múltiplos de 4
+    dd MAGIC_NUMBER             ; A arquitetura 32 bits lê 4 bytes de uma vez
+    dd FLAGS                    ; Se instruções/dados começam em endereços não
+    dd CHECKSUM                 ; múltiplos de 4, então há problema de leitura
+                                ; dd é uma diretiva que armazena esses valores no ELF
+
+loader:
+    mov eax, 0xCAFEBABE         ; colocar CAFEBABABE em um registrador
+
+.loop:
+    jmp .loop                   ; sempre em loop, para manter em memória
 
